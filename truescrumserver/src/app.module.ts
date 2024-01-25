a6326c1ba33f83d50acb5a7fd7dcddfb0dab2e70
KeunChang SONG <sol2man2@gmail.com>
2024-01-26 06:08:34 +0900

    add server
    add server

---
+++ truescrumserver/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+import * as path from 'path'
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.join(__dirname, '..', '..', 'truescrumweb', 'build'),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
